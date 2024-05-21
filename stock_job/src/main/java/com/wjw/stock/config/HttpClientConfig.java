99f02c927b9608cb946e1b2b41b539f5543d3519
wjw <Wangjingwei0@foxmail.com>
2024-05-21 15:38:56 +0800

    stock_job 环境配置、A股大盘数据采集服务

---
+++ stock_job/src/main/java/com/wjw/stock/config/HttpClientConfig.java
@@ -0,0 +1,18 @@
+package com.wjw.stock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpClientConfig {
+    /**
+     * 定义restTemplate bean
+     *
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ stock_job/src/main/java/com/wjw/stock/service/StockTimerTaskService.java
@@ -0,0 +1,8 @@
+package com.wjw.stock.service;
+
+public interface StockTimerTaskService {
+    /**
+     * 获取国内大盘的实时数据信息
+     */
+    void getInnerMarketInfo();
+}
