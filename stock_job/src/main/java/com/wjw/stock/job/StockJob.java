64baa550d82adbcd83295d4f6edfeccf3558ffe2
wjw <Wangjingwei0@foxmail.com>
2024-05-23 11:32:53 +0800

    国内大盘信息周期采集

--- stock_job/src/main/java/com/wjw/stock/job/StockJob.java
+++ stock_job/src/main/java/com/wjw/stock/job/StockJob.java
@@ -1,15 +1,23 @@
 package com.wjw.stock.job;
 
+import com.wjw.stock.service.StockTimerTaskService;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class StockJob {
+    @Autowired
+    private StockTimerTaskService stockTimerTaskService;
     @XxlJob("wjw_job_test")
     public void jobTest() {
         log.info("当前时间为: {}", DateTime.now());
+    }
+    @XxlJob("getInnerMarketInfo")
+    public void getInnerMarketInfo() {
+        stockTimerTaskService.getInnerMarketInfo();
     }
 }
